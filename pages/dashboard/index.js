386176cf56575a2c2d06aa0fbaee200c51e09752
ajitbamane05 <ajitbamane05@gmail.com>
2023-10-19 15:41:27 +0530

    dashboared updated

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
+const Jwt = require('jsonwebtoken')
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import ChatUserList from '@/Component/ChatUserList';
-const Jwt = require('jsonwebtoken')
 import axios from 'axios'
 import PrimarySearchAppBar from "@/Component/PrimarySearchAppBar";
 import Stack from '@mui/material/Stack';
-const Dadhboard = ({ data, username, users }) => {
-  const rooms = data.map((room) => room)
+const Dashboard = ({ data, username, users }) => {
+  // const rooms = data.map((room) => room)
   return (
     <div>
       <Box sx={{ display: 'flex' }}>
@@ -21,7 +21,7 @@ const Dadhboard = ({ data, username, users }) => {
           <div style={{ width: '100vw' }}>
             <Stack direction='row' spacing={2}>
               <Stack>
-                <ChatUserList rooms={rooms} users={users} />
+                <ChatUserList rooms={data} users={users} />
               </Stack>
               <Stack>
 
@@ -37,10 +37,10 @@ const Dadhboard = ({ data, username, users }) => {
   );
 }
 
-export default Dadhboard;
+export default Dashboard;
 
 export async function getServerSideProps(context) {
-  const token = context.req.cookies.token || null;
+  const token = await context.req.cookies.token || null;
   if (token) {
     const actualToken = token.split(' ')[1]
     const data1 = Jwt.verify(actualToken, process.env.SECRET)
