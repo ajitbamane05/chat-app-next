ec7a6e20ac6c77af28107c4bcc613367057ea994
ajitbamane05 <ajitbamane05@gmail.com>
2023-10-20 15:23:36 +0530

    socket.io updated

--- pages/dashboard/chat/[chatId].js
+++ pages/dashboard/chat/[chatId].js
@@ -15,7 +15,7 @@ const Chat = ({ senderId, chatId, chats, data, users, username, headers }) => {
     const [chat, setChat] = useState([...chats])
     const [socket, setSocket] = useState(null);
     useEffect(() => {
-        const socketInstance = io('http://localhost:3002');
+        const socketInstance = io('https://chat-app-pro.site/socket.io/');
         socketInstance.emit('joinRoom', chatId, senderId);
         socketInstance.on('chat', (payload) => {
             setChat((chat) => [...chat, payload])
